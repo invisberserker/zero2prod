235bfd170d92502705ba99592c92d44218150e46
invisberserker <217487548+invisberserker@users.noreply.github.com>
2025-08-26 00:13:55 +0900

    -> Ch. 7.3.8.1 - test discovery; sharing test helpers; sharing startup logic

--- src/startup.rs
+++ src/startup.rs
@@ -1,10 +1,37 @@
+use crate::configuration::Settings;
 use crate::email_client::EmailClient;
 use crate::routes::{health_check, subscribe};
 use actix_web::web::Data;
 use actix_web::{App, HttpServer, dev::Server, web};
 use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
 use std::net::TcpListener;
 use tracing_actix_web::TracingLogger;
+
+pub async fn build(configuration: Settings) -> Result<Server, std::io::Error> {
+    let connection_pool = PgPoolOptions::new()
+        // `connect_lazy_with` instead of `connect_lazy`
+        .connect_lazy_with(configuration.database.with_db());
+
+    let sender_email = configuration
+        .email_client
+        .sender()
+        .expect("Invalid sender email address.");
+    let timeout = configuration.email_client.timeout();
+    let email_client = EmailClient::new(
+        configuration.email_client.base_url,
+        sender_email,
+        configuration.email_client.authorization_token,
+        timeout,
+    );
+
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
+    let listener = TcpListener::bind(address)?;
+    run(listener, connection_pool, email_client)
+}
 
 pub fn run(
     listener: TcpListener,
