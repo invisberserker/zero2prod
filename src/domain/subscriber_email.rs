c35277da02844d2bbeff878a0528658450f3b812
invisberserker <217487548+invisberserker@users.noreply.github.com>
2025-08-26 19:23:32 +0900

    -> Ch. 7.3, refactor and cleanup

--- src/domain/subscriber_email.rs
+++ src/domain/subscriber_email.rs
@@ -49,7 +49,6 @@ mod tests {
 
     #[quickcheck_macros::quickcheck]
     fn valid_emails_are_parsed_successfully(valid_email: ValidEmailFixture) -> bool {
-        dbg!(&valid_email.0);
         SubscriberEmail::parse(valid_email.0).is_ok()
     }
 
