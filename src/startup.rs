c8be60d6834486a58d37473ce2d04e69d2d7434e
invisberserker <217487548+invisberserker@users.noreply.github.com>
2025-10-01 16:02:09 +0900

    -> Ch. 10.6 - delete a cookie; cookie security

--- src/startup.rs
+++ src/startup.rs
@@ -3,9 +3,12 @@ use crate::email_client::EmailClient;
 use crate::routes::{
     confirm, health_check, home, login, login_form, publish_newsletter, subscribe,
 };
+use actix_web::cookie::Key;
 use actix_web::web::Data;
 use actix_web::{App, HttpServer, dev::Server, web};
-use secrecy::SecretString;
+use actix_web_flash_messages::FlashMessagesFramework;
+use actix_web_flash_messages::storage::CookieMessageStore;
+use secrecy::{ExposeSecret, SecretString};
 use sqlx::PgPool;
 use sqlx::postgres::PgPoolOptions;
 use std::net::TcpListener;
@@ -77,8 +80,12 @@ pub fn run(
     // Client itself is wrapped with Arc internally to be shareable, see Ch. 7.2.2.3
     let email_client = Data::new(email_client);
     let base_url = Data::new(ApplicationBaseUrl(base_url));
+    let message_store =
+        CookieMessageStore::builder(Key::from(hmac_secret.expose_secret().as_bytes())).build();
+    let message_framework = FlashMessagesFramework::builder(message_store).build();
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(message_framework.clone())
             .wrap(TracingLogger::default())
             .route("/", web::get().to(home))
             .route("/health_check", web::get().to(health_check))
